9fce1d28d9295a9c8425deecf9fe8d5244c95cae
A2lam <mohamedallam31@hotmail.com>
2018-07-24 03:41:34 +0200

    Modification mineur

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import config from 'config';
 import express from 'express';
 import bodyParser from 'body-parser';
+import helmet from 'helmet';
+
 
 const app = express();
 
+app.use(helmet());
 app.use(bodyParser.json());
 
 app.listen(config.get('port'));
