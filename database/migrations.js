3648aed2ca930dce6c31f8d2e55fd0e54c04f361
A2lam <mohamedallam31@hotmail.com>
2018-07-25 07:44:30 +0200

    Configuration de la connexion à la bdd

---
+++ database/migrations.js
@@ -0,0 +1,5 @@
+import database from '../database';
+
+database
+    .mongodb()
+    .then(db => db.collection('users').createIndex({ firstName: 'text', lastName: 'text'}, { default_language: none }));
