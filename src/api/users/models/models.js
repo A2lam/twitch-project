dafd08a94a56d4c10caf218d89e22a1191f3d540
A2lam <mohamedallam31@hotmail.com>
2018-08-17 23:23:51 +0200

    User : Mise à jour du modèle principal des utilisateurs

--- src/api/users/models/models.js
+++ src/api/users/models/models.js
@@ -1,7 +1,11 @@
 import joi from 'joi';
 
 const model = joi.object().keys({
-  name: joi.string().required(),
+  firstName: joi.string().required(),
+  lastName: joi.string().required(),
+  email: joi.string().email().required(),
+  password: joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+  token: joi.string(),
 });
 
 export default model;
