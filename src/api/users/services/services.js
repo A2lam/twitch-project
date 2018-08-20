de91e77b3f1bc54a019a5361c410a32b0ce14fde
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2018-08-20 23:45:06 +0200

    User : Ajout des méthode de recherche, maj et suppression aux services utilisateurs

--- src/api/users/services/services.js
+++ src/api/users/services/services.js
@@ -1,43 +1,76 @@
 import joi from 'joi';
 import { ObjectId } from 'mongodb';
 import client from '../../../../database';
-import model from '../models/models';
+import model, { modelForUpdate, modelForDisplay } from '../models/models';
 
-class UsersServices
-{
-  constructor(collectionName)
-  {
+class UsersServices {
+  constructor(collectionName) {
     this.COLLECTION_NAME = collectionName;
   }
 
-  find(first = 20, offset = 0, term)
-  {
+  find(first = 20, offset = 0, term) {
     return client.mongodb()
-      .then((db) =>
-      {
+      .then((db) => {
         return db
           .collection(this.COLLECTION_NAME)
           .find(term ? { $text: { $search: term } } : null,
-          {
-            _id: 1, firstName: 1, lastNmae: 1, email: 1,
-          })
+            {
+              _id: 1, firstName: 1, lastNmae: 1, email: 1,
+            })
           .skip(offset)
           .limit(first)
-          .toArray()
-        ;
-      })
-    ;
+          .toArray();
+      });
   }
 
-  createOne(data)
-  {
-    return joi.validate(data, model).then((validatedData) =>
-    {
+  findOne(id) {
+    return joi.validate(id, joi.string().required())
+      .then(() => client.mongodb())
+      .then(db => db.collection(this.COLLECTION_NAME).findOne({ _id: ObjectId(id) }))
+      .then((element) => {
+        if (!element) throw errorMessage;
+
+        return joi.validate(element, modelForDisplay, { stripUnknown: true }).value;
+      });
+  }
+
+  createOne(data) {
+    return joi.validate(data, model).then((validatedData) => {
       client.mongodb()
-        .then((db) => db.collection(this.COLLECTION_NAME).insertOne(validatedData))
-        .then(response => response.ops[0])
-      ;
+        .then(db => db.collection(this.COLLECTION_NAME).insertOne(validatedData))
+        .then(response => response.ops[0]);
     });
+  }
+
+  updateOne(id, data) {
+    return joi.validate(id, joi.string().required())
+      .then(() => joi.validate(data, modelForUpdate))
+      .then((validatedData) => {
+        return client.mongodb()
+          .then((db) => {
+            db.collection(this.COLLECTION_NAME).updateOne(
+              { _id: ObjectId(id) },
+              { $set: validatedData },
+            );
+          });
+      })
+      .then((response) => {
+        if (response.matchedCount === 0) throw errorMessage;
+
+        return response;
+      })
+      .then(() => this.findOne(id));
+  }
+
+  deleteOne(id) {
+    return joi.validate(id, joi.string().required())
+      .then(() => client.mongodb())
+      .then(db => db.collection(this.COLLECTION_NAME).deleteOne({ _id: ObjectId(id) }))
+      .then((response) => {
+        if (response.deletedCount === 0) throw errorMessage;
+
+        return response;
+      });
   }
 }
 
