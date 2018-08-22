fb70b14a1c0cd182cdf5dd826f6633cb256664d1
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2018-08-22 23:49:20 +0200

    Auth : Ajout d'un use au point d'entrée de l'api pour la prise en compte de la nouvelle route /login

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userRooter from './users';
+import authRouter from './auth/auth';
 
 const router = Router();
 
 router.use(userRooter);
+router.use(authRouter);
 
 export default router;
