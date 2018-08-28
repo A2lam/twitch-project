849e388a9a6cff21bdbbce920193ecee318580bb
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2018-08-28 23:04:29 +0200

    Twitch : Ajout de la prise en compte de la route de récuperation des favoris au point d'entrée

--- src/api/twitch/index.js
+++ src/api/twitch/index.js
@@ -2,11 +2,13 @@ import passport from 'passport';
 import { Router } from 'express';
 
 import search from './middleware/search';
+import find from './middleware/find';
 import addFav from './middleware/addFav';
 
 const router = Router();
 
 router.get('/search', passport.authenticate('user-role', { session: false }), search);
+router.get('/favorites', passport.authenticate('user-role', { session: false }), find);
 router.post('/favorites/:game_id', passport.authenticate('user-role', { session: false }), addFav);
 
 export default router;
