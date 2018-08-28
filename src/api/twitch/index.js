1bb6585067a68a65afb8c5cd843c80c923bfcb40
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2018-08-28 23:46:13 +0200

    Twitch : Prise en charge de la route de supression d'un favori

--- src/api/twitch/index.js
+++ src/api/twitch/index.js
@@ -5,6 +5,7 @@ import search from './middleware/search';
 import find from './middleware/find';
 import addFav from './middleware/addFav';
 import findOneGame from './middleware/findOneGame';
+import deleteByGameIdAndUser from './middleware/deleteByGameIdAndUser';
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.get('/search', passport.authenticate('user-role', { session: false }), se
 router.get('/favorites', passport.authenticate('user-role', { session: false }), find);
 router.post('/favorites/:game_id', passport.authenticate('user-role', { session: false }), addFav);
 router.get('/favorites/:game_id', passport.authenticate('user-role', { session: false }), findOneGame);
+router.delete('/favorites/:game_id', passport.authenticate('user-role', { session: false }), deleteByGameIdAndUser);
 
 export default router;
