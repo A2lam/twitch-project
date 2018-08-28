d25b04f3156ae3cf8e91c9b96f900852326dcd7d
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2018-08-28 21:05:21 +0200

    Twitch : Sécurisation de la route de recherche d'élément dans twitch

--- src/api/twitch/index.js
+++ src/api/twitch/index.js
@@ -1,9 +1,10 @@
+import passport from 'passport';
 import { Router } from 'express';
 
 import search from './middleware/search';
 
 const router = Router();
 
-router.get('/search', search);
+router.get('/search', passport.authenticate('user-role', { session: false }), search);
 
 export default router;
