1122838c913000395c41362ba181146e6c033adc
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2018-08-28 22:41:12 +0200

    Twitch : Définition d'un modèle pour les favoris

--- src/api/twitch/models/models.js
+++ src/api/twitch/models/models.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 const model = joi.object().keys({
   user_id: joi.string().required(),
-  game_id: joi.integer().required(),
+  game_id: joi.string().required(),
 });
 
 export default model;
