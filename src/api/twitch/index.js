a68e919432c39d9b0539c3fd16fca5cf345904e8
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2018-08-28 20:57:21 +0200

    Twitch : Dévolepement de la méthode de listage des donnés provenant de l'api twitch

---
+++ src/api/twitch/index.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import search from './middleware/search';
+
+const router = Router();
+
+router.get('/search', search);
+
+export default router;
---
+++ src/api/twitch/middleware/search.js
@@ -0,0 +1,12 @@
+const https = require('https');
+
+export default function (req, res, next) {
+  https.request('https://api.twitch.tv/helix/streams', { headers: { 'Client-ID': 'ohwk67zppalbhicuf3yvaule4j0fwu' } }, (res1) => {
+    res1.on('data', (chunk) => {
+      res.send(JSON.parse(chunk));
+    });
+  }).on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+    next(e);
+  }).end();
+}
