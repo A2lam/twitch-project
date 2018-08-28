3df679f3fcbb7d7831a0c2dea3c63a3a80c8e619
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2018-08-28 22:42:51 +0200

    Twitch : Ajout de la route pour l'ajout d'un favori au point d'entrée de la partie Twitch

--- src/api/twitch/index.js
+++ src/api/twitch/index.js
@@ -2,9 +2,11 @@ import passport from 'passport';
 import { Router } from 'express';
 
 import search from './middleware/search';
+import addFav from './middleware/addFav';
 
 const router = Router();
 
 router.get('/search', passport.authenticate('user-role', { session: false }), search);
+router.post('/favorites/:game_id', passport.authenticate('user-role', { session: false }), addFav);
 
 export default router;
