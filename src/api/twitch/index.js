662a0e59b7f522aabb278326636d128f3dbd0311
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2018-08-28 23:29:26 +0200

    Twitch : Ajout de la prise en compte de la route de récuperation d'un jeu en particulier au point d'entrée Twitch

--- src/api/twitch/index.js
+++ src/api/twitch/index.js
@@ -4,11 +4,13 @@ import { Router } from 'express';
 import search from './middleware/search';
 import find from './middleware/find';
 import addFav from './middleware/addFav';
+import findOneGame from './middleware/findOneGame';
 
 const router = Router();
 
 router.get('/search', passport.authenticate('user-role', { session: false }), search);
 router.get('/favorites', passport.authenticate('user-role', { session: false }), find);
 router.post('/favorites/:game_id', passport.authenticate('user-role', { session: false }), addFav);
+router.get('/favorites/:game_id', passport.authenticate('user-role', { session: false }), findOneGame);
 
 export default router;
